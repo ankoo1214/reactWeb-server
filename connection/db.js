5386ce03a26d5c021f7cedda6b3a15b222bcb93d
anand-patwa-nenosystems <146060523+anand-patwa-nenosystems@users.noreply.github.com>
2024-11-07 11:03:57 +0530

    final change

--- connection/db.js
+++ connection/db.js
@@ -1,16 +1,36 @@
+// const mongoose = require("mongoose");
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(
+//       "mongodb+srv://pataleankush12:dsufegBmtVZ523hB@reactweb-cluster.txdzl.mongodb.net/?retryWrites=true&w=majority&appName=reactWeb-cluster",
+//       {
+//         serverSelectionTimeoutMS: 30000,
+       
+//       }
+//     );
+//     console.log(`Database is connected`);
+//   } catch (error) {
+//     console.log(`Database is not connected`);
+//   }
+// };
+// module.exports = connectDB;
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
     await mongoose.connect(
       "mongodb+srv://pataleankush12:dsufegBmtVZ523hB@reactweb-cluster.txdzl.mongodb.net/?retryWrites=true&w=majority&appName=reactWeb-cluster",
       {
-        serverSelectionTimeoutMS: 30000,
-       
+        useNewUrlParser: true, // Ensures compatibility with the latest MongoDB connection string parser
+        useUnifiedTopology: true, // Enables the unified topology layer
+        serverSelectionTimeoutMS: 30000, // 30 seconds timeout
       }
     );
     console.log(`Database is connected`);
   } catch (error) {
-    console.log(`Database is not connected`);
+    console.error(`Database is not connected`, error);
   }
 };
+
 module.exports = connectDB;
+
