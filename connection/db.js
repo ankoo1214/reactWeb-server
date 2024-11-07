b36882e742937e58ceb31a2fb931da4fcb8a6286
ankoo1214 <156495887+ankoo1214@users.noreply.github.com>
2024-11-07 13:35:32 +0530

    Update db.js

--- connection/db.js
+++ connection/db.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://pataleankush12:dsufegBmtVZ523hB@reactweb-cluster.txdzl.mongodb.net/?retryWrites=true&w=majority&appName=reactWeb-cluster",
-      {
-        serverSelectionTimeoutMS: 30000,
+// const mongoose = require("mongoose");
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(
+//       "mongodb+srv://pataleankush12:dsufegBmtVZ523hB@reactweb-cluster.txdzl.mongodb.net/?retryWrites=true&w=majority&appName=reactWeb-cluster",
+//       {
+//         serverSelectionTimeoutMS: 30000,
        
-      }
-    );
-    console.log(`Database is connected`);
-  } catch (error) {
-    console.log(`Database is not connected`);
-  }
-};
-module.exports = connectDB;
+//       }
+//     );
+//     console.log(`Database is connected`);
+//   } catch (error) {
+//     console.log(`Database is not connected`);
+//   }
+// };
+// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
