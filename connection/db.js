f55efecb7385f3bcd3544a8d4aa4b7cf29b55adb
ankush-patale-ncd <pataleankush12@gmail.com>
2024-11-07 17:06:45 +0530

    update

--- connection/db.js
+++ connection/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://pataleankush12:dsufegBmtVZ523hB@reactweb-cluster.txdzl.mongodb.net/?retryWrites=true&w=majority&appName=reactWeb-cluster",
+      // "mongodb+srv://pataleankush12:vOWL8rrELLqZo0SL@reactweb-cluster.txdzl.mongodb.net/?retryWrites=true&w=majority&appName=reactWeb-cluster",
+      "mongodb+srv://pataleankush12:vOWL8rrELLqZo0SL@form-cluster.hhv7p.mongodb.net/?retryWrites=true&w=majority&appName=form-cluster",
       {
         serverSelectionTimeoutMS: 30000,
        
